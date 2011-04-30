bd20b3abc243c539ccb81e097ead897fe4caf829
mns <pgameserver@gmail.com>
2011-04-30 04:36:51 +0430

    ZulAman

--- scripts/eastern_kingdoms/zulaman/boss_akilzon.cpp
+++ scripts/eastern_kingdoms/zulaman/boss_akilzon.cpp
@@ -85,17 +85,16 @@ struct MANGOS_DLL_DECL boss_akilzonAI : public ScriptedAI
         m_uiBerserkTimer = MINUTE*8*IN_MILLISECONDS;
         m_bIsBerserk = false;
 
-        m_pInstance->SetData(TYPE_AKILZON, NOT_STARTED);
+        if (m_pInstance)
+            m_pInstance->SetData(TYPE_AKILZON, NOT_STARTED);
     }
 
     void Aggro(Unit* pWho)
     {
         DoScriptText(SAY_AGGRO, m_creature);
 
-        if (!m_pInstance)
-            return;
-
-        m_pInstance->SetData(TYPE_AKILZON, IN_PROGRESS);
+        if (m_pInstance)
+            m_pInstance->SetData(TYPE_AKILZON, IN_PROGRESS);
     }
 
     void KilledUnit(Unit* pVictim)
